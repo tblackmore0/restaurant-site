c3af47af9cf920d8813e0d06cc4d5cdbb94cf59c
Tom <tblackmore0@gmail.com>
2021-04-27 13:40:05 +0100

    final transition tweaks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import {contactPageLoad} from './contact.js';
 import {menuPageLoad, hideMenu} from './menu.js';
 import {homePageLoad} from './home.js';
  
+var img1 = new Image();
+var img2 = new Image();
+var img3 = new Image();
+img1.src='./images/menu.jpeg';
+img2.src='./images/contact.jpeg';
+img3.src='./images/homepage.jpeg';
+
 initialLoad();
 
 let contactButton = document.getElementById('contactLinkButton');
